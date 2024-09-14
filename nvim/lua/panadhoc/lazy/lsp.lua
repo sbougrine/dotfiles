e053b52b460061cf05add5e9634e48e04575dee2
Souhail Bougrine <souhail.bougrine.tn@gmail.com>
2024-09-14 11:23:29 +0200

    Create lsp.lua

---
+++ nvim/lua/panadhoc/lazy/lsp.lua
@@ -0,0 +1,135 @@
+return {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        -- LSP Support
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'hrsh7th/cmp-cmdline'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'j-hui/fidget.nvim'},
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+    },
+    config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "pylsp",
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+
+                ["pylsp"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.pylsp.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            pylsp = {
+                                plugins = {
+                                    -- formatter options
+                                    black = { 
+                                        enabled = true,
+                                        line_length = 79
+                                    },
+                                    autopep8 = { enabled = false },
+                                    yapf = { enabled = false },
+                                    
+                                    -- linter options
+                                    -- pylint = { enabled = false, executable = "pylint" },
+                                    -- pyflakes = { enabled = true },
+                                    -- pycodestyle = { enabled = false },
+                                    
+                                    -- type checker
+                                    pylsp_mypy = { enabled = true },
+                                    -- auto-completion options
+                                    jedi_completion = { fuzzy = true },
+                                    -- import sorting
+                                    pyls_isort = { enabled = true },
+                                },
+                            }
+                        }
+                    })
+                end,
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+            virtual_text = {
+                prefix = ''
+            }
+
+        })
+        local signs = { Error = " ", Warn = " ", Hint = "H", Info = "I" }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+    end
+}
