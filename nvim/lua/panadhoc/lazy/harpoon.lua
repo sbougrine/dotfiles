4cd183f08b5ff9e58aaef002596b28d960194351
Souhail Bougrine <souhail.bougrine.tn@gmail.com>
2024-09-26 11:06:38 +0200

    Update harpoon.lua

--- nvim/lua/panadhoc/lazy/harpoon.lua
+++ nvim/lua/panadhoc/lazy/harpoon.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         local harpoon = require("harpoon")
         harpoon:setup()
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>l", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
